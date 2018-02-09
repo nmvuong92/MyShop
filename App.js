73fba6606660f07ef38f4a11654e32e9c9e7fb1d
ng-vượng <nmvuong92@gmail.com>
2018-02-09 11:44:22 +0700

    yaya

--- App.js
+++ App.js
@@ -14,9 +14,9 @@ import {
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev messsnu',
+    'zzz Cmd+D or shake for dev messsnu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev mssssenu',
+    'cShake or press menu button for dev mssssenu',
 });
 
 type Props = {};
@@ -25,10 +25,10 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native 2!
+          Welcome to React Native 2!zz
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js 2
+          To get started, edit App.js 2zz
         </Text>
         <Text style={styles.instructions}>
           {instructions}
